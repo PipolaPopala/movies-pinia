f13982a325f86b3d4f899b1235bcb96a1feb4a78
Evgenii Lepekho <lepehun@mail.ru>
2024-05-21 11:07:29 +0200

    refactor: refactor pinia to composition api style and add localeStorage save data

--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -1,25 +1,59 @@
+// import { defineStore } from "pinia";
+// import { useMovieStore } from "./MovieStore";
+// const url =
+//   "https:api.themoviedb.org/3/search/movie?api_key=5cbd750a3e912b835275eea63a4c35cb&query=";
+
+// export const useSearchStore = defineStore("searchStore", {
+//   state: () => ({
+//     loader: false,
+//     movies: [],
+//   }),
+//   actions: {
+//     async getMovies(search) {
+//       this.loader = true;
+//       const res = await fetch(`${url}${search}`);
+//       const data = await res.json();
+//       this.movies = data.results;
+//       this.loader = false;
+//     },
+//     addToUserMovies(object) {
+//       const movieStore = useMovieStore();
+//       movieStore.movies.push({ ...object, isWatched: false });
+//       movieStore.activeTab = 1;
+//     },
+//   },
+// });
+
+// composition api
 import { defineStore } from "pinia";
 import { useMovieStore } from "./MovieStore";
-const url =
-  "https:api.themoviedb.org/3/search/movie?api_key=5cbd750a3e912b835275eea63a4c35cb&query="; // работает только с vpn, передалаю на другое апи позже
+import { ref } from "vue";
 
-export const useSearchStore = defineStore("searchStore", {
-  state: () => ({
-    loader: false,
-    movies: [],
-  }),
-  actions: {
-    async getMovies(search) {
-      this.loader = true;
-      const res = await fetch(`${url}${search}`);
-      const data = await res.json();
-      this.movies = data.results;
-      this.loader = false;
-    },
-    addToUserMovies(object) {
-      const movieStore = useMovieStore();
-      movieStore.movies.push({ ...object, isWatched: false });
-      movieStore.activeTab = 1;
-    },
-  },
+const url =
+  "https:api.themoviedb.org/3/search/movie?api_key=5cbd750a3e912b835275eea63a4c35cb&query=";
+
+export const useSearchStore = defineStore("searchStore", () => {
+  const loader = ref(false);
+  const movies = ref([]);
+
+  const getMovies = async (search) => {
+    loader.value = true;
+    const res = await fetch(`${url}${search}`);
+    const data = await res.json();
+    movies.value = data.results;
+    loader.value = false;
+  };
+
+  const addToUserMovies = (object) => {
+    const movieStore = useMovieStore();
+    movieStore.movies.push({ ...object, isWatched: false });
+    movieStore.activeTab = 1;
+  };
+
+  return {
+    loader,
+    movies,
+    getMovies,
+    addToUserMovies,
+  };
 });
